1933ffb56411a1f14a772dcac84a2d09ac7211b0
Zack Zellto <zacharyzellto@gmail.com>
2023-05-02 17:13:40 -0600

    hooked front to back and displayed conversation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,41 @@
-import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  axios
-    .get("http://127.0.0.1:5000/ask")
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [prevAnswer, setPrevAnswer] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setQuestion(e.target.question.value);
+  };
+
+  useEffect(() => {
+    const getMessage = async () => {
+      const response = await fetch(`http://127.0.0.1:5000/ask?q=${question}`);
+      const data = await response.json();
+      setAnswer(data.answers);
+      setPrevAnswer((previous) => previous + answer + "\n");
+    };
+    question !== "" && getMessage();
+    setQuestion("");
+  }, [question]);
 
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <header className="flex flex-col items-center justify-center text-2xl">
-          <p></p>
+        <header className="flex flex-col items-center justify-center text-1xl">
+          <p>
+            {prevAnswer} <br /> {answer}
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input className=" border " name="question" type="text" />
+            <input
+              className="border text-white text-3xl bg-slate-500"
+              value="Ask"
+              type="submit"
+            />
+          </form>
         </header>
       </div>
     </>
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import cross_origin
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import openai
@@ -11,23 +11,24 @@ def configuration():
 
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/ask")
-@cross_origin()
+@app.route("/ask", methods=["GET"])
 def ask():
     configuration()
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Does this work?",
+        prompt=request.args['q'],
         temperature=0.9,
-        max_tokens=400,
+        max_tokens=1000,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
         stop=[" Human:", " AI:"]
     )
     message = response.choices[0].text.strip()
+
     return {"answers": message}
 
 
