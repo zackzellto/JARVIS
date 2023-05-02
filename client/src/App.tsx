39df83bb8d1431954934bbc05d4c281c2e78632c
Zack Zellto <zacharyzellto@gmail.com>
2023-05-01 23:33:50 -0600

    connected to openai API and tested prompts, works!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import axios from "axios";
+
 function App() {
+  axios
+    .get("http://127.0.0.1:5000/ask")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <header className="flex flex-col items-center justify-center text-2xl"></header>
+        <header className="flex flex-col items-center justify-center text-2xl">
+          <p></p>
+        </header>
       </div>
     </>
   );
--- server/server.py
+++ server/server.py
@@ -1,9 +1,31 @@
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
+from dotenv import load_dotenv
+import os
+import openai
+load_dotenv()
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
-@app.route("/ask")
-def ask():
-    return {"answers": ["Hey World", "My name is", "JARVIS"]}
 
-app.run()
+@app.route("/ask")
+@cross_origin()
+def ask():
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Does this work?",
+        temperature=0.9,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    message = response.choices[0].text.strip()
+    return {"answers": message}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
