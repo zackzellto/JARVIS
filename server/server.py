2facc7dd7cedb2544f360ea64cf662ae4cb29c20
Zack Zellto <zacharyzellto@gmail.com>
2023-05-05 00:03:36 -0600

    fixed static path

--- server/server.py
+++ server/server.py
@@ -10,13 +10,13 @@ def configuration():
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-app = Flask(__name__, static_folder="./client/dist", static_url_path="/")
+app = Flask(__name__, static_folder="../client/dist", static_url_path="/")
 CORS(app)
 
 
 @app.route("/")
 def index():
-    return send_from_directory(os.path.join(os.path.abspath(os.path.dirname(__file__)), "client/dist"), "index.html")
+    return app.send_static_file("index.html")
 
 
 @app.route("/ask", methods=["GET"])
