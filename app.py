7d9a12102c07946d02c460a8638e34bf56850e38
Zack Zellto <zacharyzellto@gmail.com>
2023-05-05 00:25:55 -0600

    changed build path

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -10,7 +10,7 @@ def configuration():
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-app = Flask(__name__, static_folder="../client/dist", static_url_path="/")
+app = Flask(__name__, static_folder="./client/dist", static_url_path="/")
 CORS(app)
 
 
